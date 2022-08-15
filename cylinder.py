280022684a800bb7ed94dc84c56790f2fe4b4d09
sara-jalalelahi <110341110+sara-jalalelahi@users.noreply.github.com>
2022-08-15 09:48:39 -0700

    Delete cylinder.py

--- cylinder.py
+++
@@ -1,8 +0,0 @@
-pi=3.14
-shoae=input('please enter shoae:')
-ertefae=input('please enter ertefae:')
-masahat=pi*int(shoae)**2
-mohit=pi*int(shoae)*2
-print('hajm =',(masahat)*int(ertefae)) 
-print('masahat janebi =',2*pi*int(shoae)*int(ertefae))
-print('masahat kol =',2*pi*int(shoae)*int(ertefae)+2*pi*int(shoae)**2)
