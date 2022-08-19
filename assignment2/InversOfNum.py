2b5b80379f9ecb32c46cb7721c1ee04c1ff0cffc
sara-jalalelahi <110341110+sara-jalalelahi@users.noreply.github.com>
2022-08-19 12:42:03 -0700

    Update InversOfNum.py

--- assignment2/InversOfNum.py
+++ assignment2/InversOfNum.py
@@ -11,32 +11,4 @@ if firstnum==container:
     print('makoos adad ba adad barabar ast')
 else:
     print('makoos adad ba adad na barabar ast')     
-
-
-
-
-
-
-
-
-# num = int(float (input('enter the  oposit number : ')))
-# number =num
-# count = 0
-
-# while (num > 0) :
-#     temp = num % 10 # 432 % 10 = 2
-#     count = (count * 10) + temp #2
-#     num //= 10    #43
-
-# if (number == count):
-#     print('Yes')
-# else:
-#     print('NO')
-
-# #tafrigh
-# sum = number + count
-
-# #jam
-# taf = number - count
-
-# print(sum,taf)
+    
