8910c0a98d1b56b137c63385ccbd8fc1cfd8ef54
sara-jalalelahi <110341110+sara-jalalelahi@users.noreply.github.com>
2022-08-21 12:37:40 -0700

    Add files via upload

---
+++ assignment2/MostEvenOddDigit.py
@@ -0,0 +1,20 @@
+adad=(input('enter number :'))
+odd=0
+even=0
+while adad !=0 :
+    lastdig=int(adad)%10
+    if lastdig%2==0:
+        even=even+1
+    else : 
+        odd=odd+1 
+    adad=int(adad)//10
+print('even digit(s) :',even, '  and  ' ,'odd digit(s) :',odd)        
+
+if even > odd:
+    print('even digits are more')
+elif even==odd:
+    print('odd and even digits are equal ')
+else:
+      print('odd digits are more')   
+
+
