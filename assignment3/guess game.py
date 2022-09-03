a5ed145e8449b21c70fee535f20cf9e852a90b2a
sara-jalalelahi <110341110+sara-jalalelahi@users.noreply.github.com>
2022-09-03 10:18:45 -0700

    Add files via upload

---
+++ assignment3/guess game.py
@@ -0,0 +1,67 @@
+# import random
+# tekrar=0
+# my_num=21
+# while True:
+#   computer_guess=random.randint(0,99)
+#   print( computer_guess)
+#   if computer_guess==my_num:
+#      print('well done')
+#      break
+ 
+#   elif computer_guess >= my_num:
+#      print('lower') 
+#      tekrar+=1 
+#      continue
+
+#   else:
+#       print('higher')
+#       tekrar+=1
+#       continue
+  
+# print('number of guesses=',tekrar)    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+import random
+while True:
+ guess=random.randint(0,99)
+ print('computer guess=',guess)
+ vroodi=int(input('1=greater\n2=lower\n3=yes ,its true\n'))
+ if vroodi==1:
+    guess1=random.randint(guess,99)
+    print('computer gueses=',guess1)
+    vroodi=int(input('1=greater\n2=lower\n3=yes ,its true\n'))
+    guess=guess1
+    print('guess1=',guess1)
+    
+ if vroodi==2:
+    guess2=random.randint(0,guess)
+    print('computer guesss=',guess2)
+    vroodi=int(input('1=greater\n2=lower\n3=yes ,its true\n'))
+    guess=guess2
+    print('guess2=',guess2)
+    
+ if vroodi==3:
+    print('well done!!!')
+    break
+
+
+
+  
+ 
+ 
