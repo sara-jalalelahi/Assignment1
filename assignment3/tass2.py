f60d15c195826a3efc5c2c910749a39b15b72e42
sara-jalalelahi <110341110+sara-jalalelahi@users.noreply.github.com>
2022-09-03 10:15:09 -0700

    Add files via upload

---
+++ assignment3/tass2.py
@@ -0,0 +1,14 @@
+import random
+container=0
+tass=random.randint(1,6)
+print('tass=',tass)
+if tass==6:
+    tass2=random.randint(1,6) 
+    while tass2==6:
+      tass2=random.randint(1,6)   
+      print('jayezeh=',tass2)
+    jam=tass+tass2
+    print('jayezeh=',tass2)
+    print('jam=',jam)
+    
+
